892fcb6fb1193e03c71a73bf110811843f2c534f
HyunsooKim <khs0415p@naver.com>
2024-01-18 20:49:21 +0900

    Modify: DB 설정 변경

--- instagram/settings.py
+++ instagram/settings.py
@@ -79,8 +79,13 @@ WSGI_APPLICATION = 'instagram.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'HOST' : '3.80.197.87',
+        'NAME': 'devops',
+        'USER': 'root',
+        'PASSWORD': 'password',
+        'PORT': '3306',
+        'OPTIONS': {'charset': 'utf8mb4'},
     }
 }
 
